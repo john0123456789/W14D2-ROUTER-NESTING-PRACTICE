0ea1d3338f1e1cf6539f46bb65ff2028f47eb8e6
john0123456789 <johnluongpham@yahoo.com>
2022-06-14 15:15:50 -0700

    ROUTER NESTING PRACTICE

--- src/components/MovieDetails/index.js
+++ src/components/MovieDetails/index.js
@@ -1,7 +1,16 @@
-function MovieDetails() {
+import { useParams } from "react-router-dom";
+
+function MovieDetails(props) {
+  const { movieId } = useParams();
+
+  console.log(useParams());
+
+  const movieChoice = props.movies.find((movie) => movie.id === +movieId);
+
   return (
-    <div className='comp purple'>
-      <h1>MovieDetails Component</h1>
+    <div className="comp purple">
+      <h1>{movieChoice.title}</h1>
+      <p>{movieChoice.description}</p>
     </div>
   );
 }
--- src/components/Movies/index.js
+++ src/components/Movies/index.js
@@ -1,7 +1,21 @@
-function Movies() {
+import { NavLink, Switch, Route } from "react-router-dom";
+import MovieDetails from "../MovieDetails";
+
+function Movies({ movies }) {
   return (
-    <div className='comp orange'>
-      <h1>Movies Component</h1>
+    <div className="comp orange">
+      <nav>
+        {movies.map((movie) => (
+          <span key={movie.id}>
+            <NavLink to={`/movies/${movie.id}`}>{movie.title}</NavLink> |
+          </span>
+        ))}
+      </nav>
+      <Switch>
+        <Route path="/movies/:movieId">
+          <MovieDetails movies={movies} />
+        </Route>
+      </Switch>
     </div>
   );
 }
